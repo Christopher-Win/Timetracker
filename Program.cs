03e8ca0053afee5e904a0758c7ab283dd35d1309
Christopher-Win <chriskhoa@gmail.com>
2024-09-08 18:23:06 -0500

    Use swagger

--- Program.cs
+++ Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddDbContext<ApplicationDBContext>(options => // This should re
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), options => options.EnableRetryOnFailure()));
 
 var app = builder.Build(); 
+app.UseSwagger();
 app.UseSwaggerUI(c =>
 {
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
